92b76a6556512a24877d2c6baf422600e49553b8
Vraj Maheshwari <vraj9081354565@gmail.com>
2026-05-05 20:05:07 +0530

    chamges run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 
 app = create_app()
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=app.config.get("DEBUG", False))
+# if __name__ == "__main__":
+#     port = int(os.getenv("PORT", "5000"))
+#     app.run(host="0.0.0.0", port=port, debug=app.config.get("DEBUG", False))
